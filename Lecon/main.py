985041d047711ce949228ce240afa41d0c9014cb
LaCapitainerie <hugo.antreassian@gmail.com>
2024-10-10 09:58:39 +0200

    Added Numpy and Refactor

--- Lecon/main.py
+++ Lecon/main.py
@@ -1,137 +1,225 @@
-from itertools import product
-import json
+from collections import deque
+from collections.abc import Generator
+from json import dump
+from typing import Optional
+from numpy import ndarray, zeros
 
 class Node:
-    next_grid:int
+    next_grid:Optional[int]
     current_player:str
-    move:tuple[int, int]
-    children:list['Node']
+    upperGrid:Optional[int]
+    subGrid:Optional[int]
+    children:deque['Node']
 
-    def __init__(self, move, current_player, next_grid):
-        self.move = move
+    def __init__(self,
+        upperGrid:Optional[int],
+        subGrid:Optional[int],
+        current_player:str,
+        next_grid:Optional[int],
+    ):
+        self.upperGrid = upperGrid
+        self.subGrid = subGrid
         self.current_player = current_player
         self.next_grid = next_grid
-        self.children = []
+        self.children = deque()
 
     def to_dict(self):
         return {
+            'turn': self.current_player,
             'winner': None,
-            'moves': {str(child.move): child.to_dict() for child in self.children}
+            'moves': {f"{child.upperGrid}-{child.subGrid}": child.to_dict() for child in self.children}
         }
 
-class Morpion:
-    def __init__(self):
-        self.board = [['' for _ in range(9)] for _ in range(9)]
-        self.super_board = ['' for _ in range(9)]
-        self.current_player = 'X'
-        self.next_grid = None
-
-    def print_board(self):
-        for i in range(9):
-            if i % 3 == 0 and i != 0:
-                print("-" * 21)
-            for j in range(9):
-                if j % 3 == 0 and j != 0:
-                    print("|", end=" ")
-                print(self.board[i][j] if self.board[i][j] else '.', end=" ")
-            print()
-        print()
-
-    def check_winner(self, grid: list[str]) -> str:
-        lines = [
-            [grid[0], grid[1], grid[2]],
-            [grid[3], grid[4], grid[5]],
-            [grid[6], grid[7], grid[8]],
-            [grid[0], grid[3], grid[6]],
-            [grid[1], grid[4], grid[7]],
-            [grid[2], grid[5], grid[8]],
-            [grid[0], grid[4], grid[8]],
-            [grid[2], grid[4], grid[6]],
-        ]
-        for line in lines:
-            if line[0] == line[1] == line[2] and line[0] != '':
-                return line[0]
-            
-        if all(cell != '' for cell in grid):
-            return 'Stale'
-        
-        return ""
     
-    def getPossibleMoves(self) -> list[tuple[int, int]]:
+def check_winner(grid: ndarray, index: int) -> str:
+    """
+    Check if there is a winner in the grid
+
+    Args:
+        grid (ndarray): The grid to check
+        index (int): The index of the grid
+
+    Returns:
+        str: The winner if there is one, else an empty string
+    """
+
+    match index:
+        case 0:
+            if grid[0] == grid[1] == grid[2] != '':
+                return grid[0]
+            if grid[0] == grid[3] == grid[6] != '':
+                return grid[0]
+            if grid[0] == grid[4] == grid[8] != '':
+                return grid[0]
+        case 1:
+            if grid[0] == grid[1] == grid[2] != '':
+                return grid[0]
+            if grid[1] == grid[4] == grid[7] != '':
+                return grid[1]
+        case 2:
+            if grid[0] == grid[1] == grid[2] != '':
+                return grid[0]
+            if grid[2] == grid[5] == grid[8] != '':
+                return grid[2]
+            if grid[2] == grid[4] == grid[6] != '':
+                return grid[2]
+        case 3:
+            if grid[3] == grid[4] == grid[5] != '':
+                return grid[3]
+            if grid[0] == grid[3] == grid[6] != '':
+                return grid[0]
+        case 4:
+            if grid[3] == grid[4] == grid[5] != '':
+                return grid[3]
+            if grid[1] == grid[4] == grid[7] != '':
+                return grid[1]
+            if grid[0] == grid[4] == grid[8] != '':
+                return grid[0]
+            if grid[2] == grid[4] == grid[6] != '':
+                return grid[2]
+        case 5:
+            if grid[3] == grid[4] == grid[5] != '':
+                return grid[3]
+            if grid[2] == grid[5] == grid[8] != '':
+                return grid[2]
+        case 6:
+            if grid[6] == grid[7] == grid[8] != '':
+                return grid[6]
+            if grid[0] == grid[3] == grid[6] != '':
+                return grid[0]
+            if grid[6] == grid[4] == grid[2] != '':
+                return grid[6]
+        case 7:
+            if grid[6] == grid[7] == grid[8] != '':
+                return grid[6]
+            if grid[1] == grid[4] == grid[7] != '':
+                return grid[1]
+        case 8:
+            if grid[6] == grid[7] == grid[8] != '':
+                return grid[6]
+            if grid[2] == grid[5] == grid[8] != '':
+                return grid[2]
+            if grid[0] == grid[4] == grid[8] != '':
+                return grid[0]
+            
+    if all(cell != '' for cell in grid):
+        return 'Stale'
+    
+    return ""
+
+
+class Morpion:
+    def __init__(self,
+            board:ndarray=zeros((9, 9), dtype=str),
+            super_board:ndarray=zeros(9, dtype=str),
+            current_player:str='X',
+            next_grid:Optional[int]=None
+        ):
+        self.board = board
+        self.super_board = super_board
+        self.current_player = current_player
+        self.next_grid = next_grid
+    
+    def getPossibleMoves(self) -> Generator[tuple[int, int], None, None]:
         """
         Get all possible moves
         """
         
-        if isinstance(self.next_grid, int):
-            return [(self.next_grid, pos) for pos, j in enumerate(self.board[self.next_grid]) if j == '']
-        return [(i, j) for i in range(9) for j in range(9) if self.board[i][j] == '']
+        if self.next_grid:
+            return ((self.next_grid, pos) for pos, j in enumerate(self.board[self.next_grid]) if j == '')
+        
+        return ((i, j) for i in range(9) for j in range(9) if self.board[i][j] == '')
 
     def make_move(self, grid:int, cell:int) -> bool:
+        """
+        Make a move in the game
+        
+        Args:
+            grid (int): The grid where to play
+            cell (int): The cell where to play
+            
+        Returns:
+            bool: True if the move is valid, else False
+        """
         
         if self.board[grid][cell] != '' or (self.next_grid is not None and grid != self.next_grid):
             return False
+        
 
         self.board[grid][cell] = self.current_player
 
-        winner = self.check_winner(self.board[grid])
-
-        if winner:
+        
+        if (winner := check_winner(self.board[grid], cell)):
             self.super_board[grid] = winner
+            check_winner(self.super_board, grid)
 
         self.next_grid = cell if self.super_board[cell] == '' else None
 
         self.current_player = 'O' if self.current_player == 'X' else 'X'
+
         return True
 
-    def check_super_winner(self):
-        return self.check_winner(self.super_board)
+    def generate_move_tree(self, node:Node, depth:int, max_depth:int):
+        """
+        Generate the move tree
+        
+        Args:
+            node (Node): The node to generate the tree from
+            depth (int): The current depth of the tree
+            max_depth (int): The maximum depth of the tree
+        
+        Returns:
+            None
+        """
 
-    def generate_move_tree(self, node, depth, max_depth):
         if depth >= max_depth:
             return
 
-        winner = self.check_super_winner()
-        if winner:
-            node.winner = winner
-            return
+        # winner = self.check_super_winner()
+        # if winner:
+        #     node.winner = winner
+        #     return
 
-        possible_moves = self.getPossibleMoves() #[(i, j) for i in range(9) for j in range(9) if self.board[i][j] == '' and (self.next_grid is None or (i // 3, j // 3) == self.next_grid)]
-        # print("possibles_moves", possible_moves)
-        for move in possible_moves:
-            new_board = [row[:] for row in self.board]
-            new_super_board = self.super_board[:]
-            new_game = Morpion()
-            new_game.board = new_board
-            new_game.super_board = new_super_board
-            new_game.current_player = self.current_player
-            new_game.next_grid = self.next_grid
-            if new_game.make_move(move[0], move[1]):
-                child_node = Node(move, new_game.current_player, new_game.next_grid)
+        for (upperGrid, subGrid) in self.getPossibleMoves():
+            
+            new_game = Morpion(
+                board=self.board.copy(),
+                super_board=self.super_board.copy(),
+                current_player=self.current_player,
+                next_grid=self.next_grid
+            )
+            
+            if new_game.make_move(upperGrid, subGrid):
+
+                child_node = Node(
+                    upperGrid=upperGrid,
+                    subGrid=subGrid,
+                    current_player=new_game.current_player,
+                    next_grid=new_game.next_grid
+                )
+
                 node.children.append(child_node)
                 new_game.generate_move_tree(child_node, depth + 1, max_depth)
 
-    def play(self, max_depth=3):
-        root = Node(None, self.current_player, self.next_grid)
+    def play(self, max_depth:int=3):
+        """
+        Play the game
+        
+        Args:
+            max_depth (int): The maximum depth of the tree
+        """
+
+        root = Node(None, None, self.current_player, self.next_grid)
         self.generate_move_tree(root, 0, max_depth)
         self.save_tree(root)
 
     def save_tree(self, root):
         tree_dict = root.to_dict()
         with open('tree.json', 'w') as f:
-            json.dump(tree_dict, f, indent=4)
-
-    def load_config(self, config_path):
-        with open(config_path, 'r') as f:
-            config = json.load(f)
-        self.board = config['board']
-        self.super_board = config['super_board']
-        self.current_player = config['current_player']
-        self.next_grid = tuple(config['next_grid']) if config['next_grid'] is not None else None
+            dump(tree_dict, f, indent=1)
 
 if __name__ == "__main__":
     game = Morpion()
-    # Load configuration if needed
-    # game.load_config('config.json')
-    game.play(max_depth=5)
+    game.play(max_depth=4)
 
 
